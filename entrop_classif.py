0e40c112b03a17501294f7adfeef506a428c4ad4
Abdur-666 <105206332+Abdur-666@users.noreply.github.com>
2022-05-09 15:51:34 +0530

    Update entrop_classif.py

--- entrop_classif.py
+++ entrop_classif.py
@@ -2,11 +2,13 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression 
+from sklearn import svm
+
 
 # read data
-x1 = np.random.multivariate_normal([30,30], [[3, 0.5], [0.5, 4]], 100)
-y1= np.random.multivariate_normal([34.5,34.5], [[4,1],[1,3]], 100)
+x1 = np.random.multivariate_normal([51,54], [[3, 0.5], [0.5, 4]], 100)
+y1= np.random.multivariate_normal([49.9,47], [[4,1],[1,3]], 100)
 
 #sessioning the random data
 with open("set1.csv","r+") as set1:
@@ -23,7 +25,8 @@ y= np.array([0]*100 + [1]*100)
 log_reg = LogisticRegression()
 log_reg.fit(X, y)
 
-
+# clf = svm.SVC(kernel="linear")
+# clf = clf.fit(X,y)
 parameters = log_reg.coef_[0]
 
 parameter0 = log_reg.intercept_
@@ -33,9 +36,11 @@ fig = plt.figure()
 x_values = [np.min(X[:, 1] -5 ), np.max(X[:, 1] +5 )]
 
 y_values = np.dot((-1./parameters[1]), (np.dot(parameters[0],x_values) + parameter0))
-colors=['green' if l==0 else 'red' for l in y]
+colors=['blue' if l==0 else 'yellow' for l in y]
+f = plt.axes()
+f.set_facecolor("black")
 plt.scatter(X[:, 0], X[:, 1], label='Logistics regression', color=colors)
-plt.plot(x_values, y_values, label='Decision Boundary')
+plt.plot(x_values, y_values, label='Decision Boundary',c="white")
 plt.title("Classification of two sets of 2D Normal distribution")
 
 plt.show()
